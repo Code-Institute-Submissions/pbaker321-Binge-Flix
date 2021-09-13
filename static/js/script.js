3a08846e38daf52cb4cfa70131fa18611299148c
pbaker321 <pete.baker111@gmail.com>
2021-09-13 20:21:03 +0000

    Added Modal for Deletefunction

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {    
     $('.sidenav').sidenav();
-    $('select').formSelect();        
+    $('select').formSelect();
+    $('.modal').modal();        
 
 /*
 Taken from the course for the dropdown menu
